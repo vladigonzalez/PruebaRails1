262479108a7357ca938af1bbc73f9b15a935d651
Vladimir <Vladigonzalez@gmail.com>
2017-07-31 23:13:56 -0400

    tercer commit create

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -7,4 +7,15 @@ class TodosController < ApplicationController
     @todos = Todo.new
   end
 
+  def create
+    @todos = Todo.new(todos_params)
+    #@todos = Todo.new
+    @todos.save
+    redirect_to todos_path
+  end
+
+  private
+  def todos_params
+    params.require(:todo).permit(:description, :completed)
+  end
 end
