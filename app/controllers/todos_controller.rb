b0d5163cf3cb8977d0877c6ac44afb6642e1b33f
Vladimir <Vladigonzalez@gmail.com>
2017-07-31 22:41:44 -0400

    segundo commit new

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,10 @@
 class TodosController < ApplicationController
+  def index
+      @todos = Todo.all
+  end
+
+  def new
+    @todos = Todo.new
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'todos', to: 'todos#index'
+  get 'todos/new'
 
+  get 'todos', to: 'todos#create'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
